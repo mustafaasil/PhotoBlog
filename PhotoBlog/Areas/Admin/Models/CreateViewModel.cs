2881af3a3650649c5cf7a40d2f2c394f57208ce4
Mustafa <mustafa.asill@hotmail.com>
2023-01-16 11:37:37 +0300

    Add project files.

---
+++ PhotoBlog/Areas/Admin/Models/CreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PhotoBlog.Attributes;
+
+namespace PhotoBlog.Areas.Admin.Models
+{
+    public class CreateViewModel
+    {
+
+        [MaxLength(140)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(400)]
+        public string? Description { get; set; }
+
+        [ValidImage(MaxFileSize = 2)]
+        public IFormFile Photo { get; set; } = null!;
+
+        public HashSet<string>? Tags { get; set; } = new HashSet<string>();
+    }
+}
---
+++ PhotoBlog/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhotoBlog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<Tag> Tags => Set<Tag>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(
+                new Post()
+                {
+                    Id = 1,
+                    Title = "Mountains and Sea",
+                    Description = "As the sun sets behind the hills, I watch the unique blue of the sea.",
+                    Photo = "sample.jpg"
+                }
+            );
+
+            modelBuilder.Entity<Tag>().HasData(
+                new Tag() { Id = 1, Name = "nature" },
+                new Tag() { Id = 2, Name = "sea" },
+                new Tag() { Id = 3, Name = "hiking" },
+                new Tag() { Id = 4, Name = "sunset" },
+                new Tag() { Id = 5, Name = "walk" },
+                new Tag() { Id = 6, Name = "rain" }
+                );
+        }
+    }
+}
