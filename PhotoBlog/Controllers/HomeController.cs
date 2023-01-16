d5223b5d7011f92ba4e49cd21be94f364d623c6c
Mustafa <mustafa.asill@hotmail.com>
2023-01-16 17:01:19 +0300

    update

--- PhotoBlog/Controllers/HomeController.cs
+++ PhotoBlog/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using PhotoBlog.Data;
 using PhotoBlog.Models;
@@ -17,13 +18,36 @@ namespace PhotoBlog.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string? q)
         {
-            var posts = _db.Posts
-                .Include(x => x.Tags)
-                .OrderByDescending(x => x.CreatedTime)
+            IQueryable<Post> query = _db.Posts
+                .Include(x => x.Tags);
+
+            if (!string.IsNullOrWhiteSpace(q))
+            {
+                q = q.Trim();
+
+                if (q.StartsWith("#"))
+                {
+                    var tag = q.Substring(1);
+                    query = query.Where(x => x.Tags.Any(t => t.Name == tag));
+                }
+                else
+                {
+                    query = query.Where(x => x.Title.Contains(q) || x.Description!.Contains(q)); 
+                }
+            }
+
+                var posts = query.OrderByDescending(x => x.CreatedTime)
                 .ToList();
-            return View(posts);
+
+            var vm = new HomeViewModel()
+            {
+                Posts = posts,
+                Search = q
+            };
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
